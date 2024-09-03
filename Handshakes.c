2e1ee37991e5ed24a5cade6bbcb06ebdbb06511e
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2024-09-03 19:28:20 +0530

    Create Handshakes.c

---
+++ Handshakes.c
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // your code goes here
+    long long n;
+    scanf("%lld",&n);
+    long long x=(n*(n-1)/2);
+    printf("%lld",x);
+    return 0;
+}
